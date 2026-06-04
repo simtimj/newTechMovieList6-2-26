6e47b4bdc6815bc29f46063b4ad369957ec5fc86
Julius Simtim <juliussimtim@gmail.com>
2026-06-03 20:43:32 -0700

    Complete level 0 reqs, along with styling

---
+++ components/MovieList.tsx
@@ -0,0 +1,34 @@
+
+interface Movie {
+  id: number
+  title: string
+}
+
+interface MovieListProps {
+  movies: Movie[]
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+  // console.log('movies:', movies);
+  return (
+    <>
+      <ul className="flex flex-col divide-y divide-gray-400 border border-gray-400 m-8"> 
+          {movies.map((movie, i) => (
+            <MovieListEntry key={movie.id} movie={movie} />
+          ))}
+      </ul>
+    </>
+  )
+}
+
+const MovieListEntry = ({ movie }: { movie: Movie }) => {
+  return (
+    <li
+      className="p-4 pl-8"
+    >{movie.title}
+    </li> 
+  )
+}
+
+export default MovieList;
+
---
+++ mockData.tsx
@@ -0,0 +1,33 @@
+
+
+interface Movie {
+  id: number
+  title: string
+}
+
+
+// array of objects
+let movies: Array<Movie> = [
+  { 
+    id: 0,
+    title: 'Mean Girls' 
+  },
+  { 
+    id: 1,
+    title: 'Hackers' 
+  },
+  {
+    id: 2,
+    title: 'The Grey' 
+  },
+  {
+    id: 3,
+    title: 'Sunshine' 
+  },
+  {
+    id: 4,
+    title: 'Ex Machina' 
+  },
+];
+
+export default movies;
