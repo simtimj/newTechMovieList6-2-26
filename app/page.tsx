127892488516c00e2d108dd79dba9a6ff33b4e93
Julius Simtim <juliussimtim@gmail.com>
2026-06-04 21:22:35 -0700

    Add working searchbar along with filtering and no movie case

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,44 @@
-import mockMovies from "../mockData"
-import MovieList from "../components/MovieList";
+'use client'
+
+import mockMovies from "@/mockData"
+import MovieList from "@/components/MovieList";
+import Search from "@/components/Search";
+import { useState } from "react";
 
 export default function Home() {
-  console.log('mockMovies:', mockMovies);
+
+  interface Movie {
+    id: number
+    title: string
+  }
+
+  
+  let [allMovies, setAllMovies] = useState<Movie[]>(mockMovies);
+  let [searchText, setSearchText] = useState<string>("");
+
+
+  // all filters
+  let filterMovies = () => {
+    // searchFilter
+    
+    let newMovies = [...allMovies];
+
+    newMovies = newMovies.filter((movie) => {
+      return (movie.title.toLowerCase()).includes(searchText.toLowerCase());
+    })
+    return newMovies;   
+  }
+
+
   return (
   <>
-    <MovieList movies={mockMovies}/>
+    <h1 className="ml-8 mt-8 text-2xl">MovieList</h1>
+    <Search 
+      setSearchText={setSearchText}
+    />
+    <MovieList 
+      movies={filterMovies().length ? filterMovies() : [{title: `No Movies matching ${searchText}`, id: 0} ]}
+    />
   </>
 )
 }
---
+++ components/Search.tsx
@@ -0,0 +1,42 @@
+
+import {useState} from "react";
+
+interface SearchProps {
+  setSearchText: (text: string) => void
+}
+
+let Search = ({setSearchText}: SearchProps) => {
+  // type into search input box
+  // on click of input button, send to MovieList for filtering
+
+  let [inputText, setInputText] = useState<string>("");
+
+
+  return (
+    <div>
+      <form
+        className="flex flex-row w-0.5 pl-4 ml-4 mt-4"
+        onSubmit={(e) => { 
+          setSearchText(inputText) 
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Search..."
+          className="border border-r-0 rounded-l-sm border-gray-400 placeholder-gray-400 px-2"
+          onChange={(e) => {setInputText(e.target.value)}}
+        />
+        <button
+          type="submit"
+          className="px-1 border rounded-r-sm border-gray-400 text-gray-700"
+        >
+          Go!
+        </button>
+
+      </form>
+    </div>
+  )
+}
+
+export default Search;
